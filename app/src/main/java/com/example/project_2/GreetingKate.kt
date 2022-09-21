12790f438c2d243b3b000ab224189ebe5940d99d
nikvladimir <alfa.voland@mail.ru>
2022-09-21 08:42:31 +0300

    added greeting Vova Kate Myself

---
+++ app/src/main/java/com/example/project_2/GreetingKate.kt
@@ -0,0 +1,11 @@
+package com.example.project_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GreetingKate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greeting_kate)
+    }
+}
--- app/src/main/java/com/example/project_2/MainActivity.kt
+++ app/src/main/java/com/example/project_2/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.project_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textVova: TextView = findViewById(R.id.greeting_vova)
+        textVova.setOnClickListener() {
+            openGreetingVovaActivity()
+        }
+    }
+
+    private fun openGreetingVovaActivity() {
+        val intent = Intent(this, GreetingVova::class.java)
+        startActivity(intent)
     }
 }
